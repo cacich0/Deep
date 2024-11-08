0446997e1403278f00c960b2f86176260458a8ea
Artemiy Kobelev <iamcacich@icloud.com>
2024-11-08 03:28:08 +0300

    Added documentation for Lazy wrappers

--- Sources/Deep/Lazy.swift
+++ Sources/Deep/Lazy.swift
@@ -1,5 +1,27 @@
 import Foundation
 
+/// A wrapper for lazy initialization of a dependency.
+/// The object is created only when it is accessed for the first time.
+///
+/// WARNING: You need to use the `Lazy` wrapper
+/// exclusively inside `WithInterface` and `WithIdentifier
+///
+/// Note: Use this only if you don't need to initialize the object immediately.
+///
+/// Example:
+///
+///     Container(\.network) {
+///         WithInterface(
+///             Lazy(Networker()),
+///             interface: NetworkService.self
+///         )
+///         WithIdentifier(
+///             identifier: "NetworkerSecond",
+///             instance: Lazy(NetworkerSecond()),
+///             interface: NetworkService.self
+///         )
+///     }
+///
 public struct Lazy {
     internal let block: () -> Any
     
@@ -8,6 +30,20 @@ public struct Lazy {
     }
 }
 
+/// A structure for lazy initialization of a dependency with a specified interface type.
+/// Used to register an object that will only be created upon first access.
+///
+/// WARNING: You need to use the `WithLazy` wrapper exclusively
+/// when you are not using `WithInterface` and `WithIdentifier`
+///
+/// Note: Use this only if you don't need to initialize the object immediately.
+///
+/// Example:
+///
+///     Container(\.network) {
+///         WithLazy(DataLoader(), type: DataLoader.self)
+///     }
+///
 public struct WithLazy {
     private let block: () -> Any
     internal let type: Any.Type
